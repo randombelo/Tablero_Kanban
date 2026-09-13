994ebb512463481c8ff38d80a2478f67edf4b620
randombelo <abelmarrerocamero@gmail.com>
2026-09-13 22:24:30 +0200

    fix: fixing url of state.js

--- src/js/state.js
+++ src/js/state.js
@@ -3,4 +3,4 @@ export const state = {
   currentTaskId: null,
   currentCommentId: null
 };
-export const API_BASE = 'https://tablero-kanbanrandombelo.onrender.com/';
+export const API_BASE = 'https://tablero-kanbanrandombelo.onrender.com';
